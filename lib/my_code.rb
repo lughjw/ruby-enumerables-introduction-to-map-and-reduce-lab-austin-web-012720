792fec2429ae2531ee00d3d792c9c0caffe58678
Lugh Wilson <lughjw@gmail.com>
2020-01-03 06:08:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,45 @@
 # My Code here....
+# Map-like methods
+def map_to_negativize(source_array)
+  # INPUT: 
+  # * source_array: Array of numbers
+  #
+  # OUTPUT: Array of the negative values of the source_array
+  retArray = []
+  index = 0
+  while index < source_array.size do
+    retArray[index] = source_array[index]*-1
+    
+    index += 1
+  end
+  
+  retArray
+end
+
+def map_to_no_change(source_array)
+  source_array
+end
+
+def map_to_double(source_array)
+  retArray = []
+  
+  index = 0
+  
+end
+
+def map_to_square(source_array)
+  
+end
+
+# Reduce-like methods
+def reduce_to_total(source_array, starting_point)
+  
+end
+
+def reduce_to_all_true(source_array)
+  
+end
+
+def reduce_to_any_true(source_array)
+  
+end
